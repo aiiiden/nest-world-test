7a827196b877f038c12bada102e7e239fffc4425
aiiiden <hjjam100@gmail.com>
2024-12-18 19:36:44 +0900

    doc: add instruction

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Nest.js + MiniKit-JS SDK Error Report
+
 해당 리포지토리는 MiniKit-JS SDK 와 Nest.js에서의 오류를 제보하기 위한 리포지토리입니다.
 
 This repository is for reporting errors in MiniKit-JS SDK and Nest.js.
+
+## How to start
+
+1. Clone this repository
+2. Install dependencies (`yarn install`)
+3. Run the server in dev mode (`yarn start:dev`)
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,12 @@ import { Tokens, tokenToDecimals } from '@worldcoin/minikit-js';
 @Injectable()
 export class AppService {
   getHello(): string {
+    // Using the tokenToDecimals function from the minikit-js package
     const decimal = tokenToDecimals(100000000, Tokens.USDCE);
+
+    /**
+     * Error: Cannot find module 'react'
+     */
 
     return 'Hello World! ' + decimal;
   }
